6003e70403da8a90d8082bae8fdab474ec575d26
macwin99 <dmurillotec@gmail.com>
2015-09-03 00:29:19 -0600

    GET Path Change
    Little change for the Path to access the GET

--- src/com/parallelmountain/rest/status/Status.java
+++ src/com/parallelmountain/rest/status/Status.java
@@ -1,2 +1,16 @@
 package com.parallelmountain.rest.status;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/status")
+public class Status {
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle() {
+		return "<p>Java Web Service</p>";
+	}
+	
+}
